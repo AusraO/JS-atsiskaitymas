bd06d9e53c94776a5f62001ffc049585854fca4e
AusraO <ausra.241@protonmail.com>
2023-04-11 09:00:37 +0300

    first commit

---
+++ Task 1/script.js
@@ -0,0 +1,30 @@
+/* ------------------------------ TASK 1 ----------------------------
+Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
+pamatyti jo pateikto svorio konvertavimą į:
+1. Svarus (lb) | Formulė: lb = kg * 2.2046
+2. Gramus (g) | Formulė: g = kg / 0.0010000
+3. Uncijos (oz) | Formulė: oz = kg * 35.274
+
+Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
+<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
+------------------------------------------------------------------- */
+
+function svorioSkaiciavimas(event) {
+
+    event.preventDefault();
+    let svorisKg = event.target.elements.search.value;
+
+    let svorisSvarais = svorisKg * 2.2046
+    let svorisGramais = svorisKg / 0.0010000
+    let svorisUncijomis = svorisKg * 35.274
+
+     document.querySelector('#output').innerHTML =
+   "Weight in lb = " + svorisSvarais +
+        "<span> Weight in g = " + svorisGramais + '</span>'+
+        "<span>Weight in oz = " + svorisUncijomis+'</span>'
+}
+
+document
+    .querySelector('form')
+    .addEventListener('submit', svorioSkaiciavimas)
+
