37a5c65fb5b5ae4dd3f4d5af8802aa989c978ce2
AusraO <ausra.241@protonmail.com>
2023-04-11 12:38:30 +0300

    task 8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,31 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+class Calculator {
+
+    sum(numX, numY) {
+        return numX + numY
+    };
+    substraction(numX, numY) {
+        return numX - numY
+    };
+    multiplication(numX, numY) {
+        return numX * numY
+    }
+    division(numX, numY) {
+        return numX / numY
+    }
+}
+
+const calculator = new Calculator();
+
+const calculatorSumExample = calculator.sum(4, 6);
+const calculatorSubstractExample = calculator.substraction(10, 7);
+const calculatorMultiplExample = calculator.multiplication(2, 5);
+const calculatorDivisionExample = calculator.division(6, 2);
+console.log(calculatorSumExample);
+console.log(calculatorSubstractExample);
+console.log(calculatorMultiplExample);
+console.log(calculatorDivisionExample);
